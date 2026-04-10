af3a63d47325e30bcc8cb26ebddb1de68d84baea
Max Rodrigues <maxuel.rodrigues@gmail.com>
2026-04-09 22:26:49 -0300

    [feat] ✨: add chart to dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Charts\WorksMunicipality;
+use App\Models\Work;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,6 +13,23 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('dashboard');
+        $works = Work::selectRaw('municipality, COUNT(1) as count')->groupBy('municipality')->get();
+        
+        $chart = new WorksMunicipality();
+
+        $chart->title('Obras Por Município');
+        $chart->options([
+            'legend' => [
+                'display' => false,
+            ],
+        ]);
+        $chart->labels($works->pluck('municipality'));
+        $chart->dataset('Obras Por Município', 'line', $works->pluck('count'))
+            ->color('rgb(255, 99, 132)')
+            ->fill(false);
+        
+        
+
+        return view('dashboard', compact('chart'));
     }
 }

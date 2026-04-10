7d470f0d75cb7e64c409194e21a82a35ef6bc579
Max Rodrigues <maxuel.rodrigues@gmail.com>
2026-04-10 09:31:40 -0300

    [feat] ✨: add route map

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProcessUploadController;
 use App\Http\Controllers\UploadController;
 use App\Http\Controllers\WorksController;
+use App\Http\Controllers\WorkMapController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', DashboardController::class)->name('dashboard');
 Route::get('/upload', UploadController::class)->name('upload');
 Route::post('/process-upload', ProcessUploadController::class)->name('process-upload');
 Route::get('/works', WorksController::class)->name('works');
+Route::get('/works-map', WorkMapController::class)->name('works-map');
